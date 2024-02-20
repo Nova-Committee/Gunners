5723301d2932488bfbf33f9754bfe22c3deed2ac
MikhailTapio <mikhailtapio13468@163.com>
2024-02-20 12:44:01 +0800

    Add a compat config

---
+++ src/main/java/team/dovecotmc/gunners/Gunners.java
@@ -0,0 +1,8 @@
+package team.dovecotmc.gunners;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class Gunners {
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
--- src/main/java/team/dovecotmc/gunners/compat/CompatHandler.java
+++ src/main/java/team/dovecotmc/gunners/compat/CompatHandler.java
@@ -1,43 +1,52 @@
 package team.dovecotmc.gunners.compat;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.apache.commons.io.FileUtils;
+import team.dovecotmc.gunners.Gunners;
 import team.dovecotmc.gunners.compat.gun.cgm.CgmHandler;
 
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 public class CompatHandler {
     private static CompatHandler instance;
+    public static final Path gunnersDir = FMLPaths.CONFIGDIR.get().resolve("gunners");
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
     public final boolean cgmLoaded;
     public final boolean recruitsLoaded;
     public final boolean guardVillagersLoaded;
 
     public CompatHandler() {
-        boolean cgm;
-        try {
+        final Config cfg = getCompatConfig();
+        boolean cgm = false;
+        if (cfg.cgm) try {
             Class.forName("com.mrcrayfish.guns.GunMod", false, this.getClass().getClassLoader());
             cgm = true;
-        } catch (Exception e) {
-            cgm = false;
+        } catch (Exception ignored) {
         }
         cgmLoaded = cgm;
-        boolean recruits;
-        try {
+        boolean recruits = false;
+        if (cfg.recruits) try {
             Class.forName("com.talhanation.recruits.Main", false, this.getClass().getClassLoader());
             recruits = true;
-        } catch (Exception e) {
-            recruits = false;
+        } catch (Exception ignored) {
         }
         recruitsLoaded = recruits;
-        boolean guardVillagers;
-        try {
+        boolean guardVillagers = false;
+        if (cfg.guardVillagers) try {
             Class.forName("tallestegg.guardvillagers.GuardVillagers", false, this.getClass().getClassLoader());
             guardVillagers = true;
         } catch (Exception ignored) {
-            guardVillagers = false;
         }
         guardVillagersLoaded = guardVillagers;
-        instance = this;
     }
 
     public static CompatHandler getInstance() {
@@ -57,5 +66,46 @@ public class CompatHandler {
     public static boolean isGun(ItemStack stack) {
         if (instance.cgmLoaded && CgmHandler.isGun(stack)) return true;
         return false;
+    }
+
+    @Nonnull
+    private static Config getCompatConfig() {
+        if (!gunnersDir.toFile().isDirectory()) {
+            try {
+                Files.createDirectories(gunnersDir);
+            } catch (IOException e) {
+                Gunners.LOGGER.error("Failed to create directory for gunner compat config!", e);
+                return new Config();
+            }
+        }
+        final Path cfgPath = gunnersDir.resolve("gunners-compat.json");
+        Config config = new Config();
+        if (cfgPath.toFile().isFile()) {
+            try {
+                config = GSON.fromJson(FileUtils.readFileToString(cfgPath.toFile(),
+                        StandardCharsets.UTF_8), Config.class);
+                return config;
+            } catch (IOException e) {
+                Gunners.LOGGER.error(String.format("Failed to parse gunners compat config \"%s\"", cfgPath), e);
+                FileUtils.deleteQuietly(cfgPath.toFile());
+                return createCompatConfig(cfgPath, config) ? config : new Config();
+            }
+        } else return createCompatConfig(cfgPath, config) ? config : new Config();
+    }
+
+    private static boolean createCompatConfig(Path cfgPath, Config config) {
+        try {
+            FileUtils.write(cfgPath.toFile(), GSON.toJson(config), StandardCharsets.UTF_8);
+            return true;
+        } catch (IOException e) {
+            Gunners.LOGGER.error(String.format("Failed to create gunners compat config \"%s\"", cfgPath), e);
+        }
+        return false;
+    }
+
+    public static class Config {
+        public boolean cgm = true;
+        public boolean recruits = true;
+        public boolean guardVillagers = true;
     }
 }
